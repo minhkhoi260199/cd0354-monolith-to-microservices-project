c5bc17ae5854fe0f2d7d1d58eb87b898ffc33a76
N3GAN\minhk <minhkhoi260199@gmail.com>
2024-04-13 09:38:02 +0700

    u

--- set_env.sh
+++ set_env.sh
@@ -1,11 +1,11 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=postgres
-export POSTGRES_HOST=database-1.cbw62njalayy.us-east-1.rds.amazonaws.com
+export POSTGRES_PASSWORD=passphientoai
+export POSTGRES_HOST=postgres.cbw62njalayy.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
 export AWS_BUCKET=arn:aws:s3:::default-348832570760-bucket
-export AWS_REGION=us-east-1c
+export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=testing
 export URL=http://localhost:8100
